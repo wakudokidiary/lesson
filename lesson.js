c515a78d6132bf9b3fba7742fc0683d837b30ae9
wakudokidiary <77178772+wakudokidiary@users.noreply.github.com>
2021-03-31 22:12:48 +0900

    Add files via upload

--- lesson.js
+++ lesson.js
@@ -14,4 +14,26 @@ $(function() {
     $('#roller').fadeIn();
   });
 
+    // ここからメニュー
+    $('#bar').click(function(){
+      $('#menu').slideDown();
+    });
+  
+    $('#bar').click(function(){
+      $(this).fadeOut();
+    });
+  
+    $('#batsu').click(function(){
+      $('#menu').slideUp();
+    });
+  
+  
+  
+    $('#batsu').click(function(){
+      $('#bar').fadeIn();
+    });
+  
+  // ここまでメニュー
+  
+
 });
